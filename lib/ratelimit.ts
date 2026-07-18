4eac68648259d1dc3ed40075593673a42c6c9961
Vaibhav Ravindra Patil <156682240+Vaibhav2024@users.noreply.github.com>
2026-07-18 17:15:55 +0530

    Change rate limit window from 1 minute to 1 hour

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -14,7 +14,7 @@ export const redis = new Redis({
 
 export const ratelimit = new Ratelimit({
   redis: redis,
-  limiter: Ratelimit.slidingWindow(5, "1m"),
+  limiter: Ratelimit.slidingWindow(5, "1h"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
